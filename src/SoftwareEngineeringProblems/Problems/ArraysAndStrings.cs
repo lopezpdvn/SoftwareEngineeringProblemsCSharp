8b81a5a596e7504bf7734c67eb9b6fcbd75bb01c
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-08-09 22:52:02 -0500

    Worked on some Arrays and Strings problems
    - ArraysAndStrings.AreCharacterSetsEqual
    - ArraysAndStrings.ReplaceSpaces0

---
+++ src/SoftwareEngineeringProblems/Problems/ArraysAndStrings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataStructures.LinkedList;
+
+namespace SoftwareEngineeringProblems
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class ArraysAndStrings
+    {
+
+        public static bool AreCharacterSetsEqual(string original, string val2Test)
+        {
+            var originalSet = new HashSet<char>(original);
+            var val2TestSet = new HashSet<char>(val2Test);
+            return originalSet.SetEquals(val2Test);
+        }
+
+        // Based on https://github.com/gaylemcd/ctci/blob/master/c-sharp/Chapter01/Q01_4.cs
+        public static void ReplaceSpaces0(char[] str, int trueLength)
+        {
+            var nSpaces = 0;
+
+            // count the number of spaces
+            foreach (var c in str)
+            {
+                if (c == ' ')
+                {
+                    nSpaces++;
+                }
+            }
+
+            // New string size. One can't assume it's str.Length,
+            // we can only say that str.Length >= (new string size)
+            var newStrSize = trueLength + (nSpaces * 2);
+
+            // copying the characters backwards and inserting %20
+            for (int i = trueLength - 1, j = newStrSize - 1; i >= 0; i--, j--)
+            {
+                if (str[i] == ' ')
+                {
+                    str[j] = '0';
+                    str[--j] = '2';
+                    str[--j] = '%';
+                }
+                else
+                {
+                    str[j] = str[i];
+                }
+            }
+        }
+    }
+}
---
+++ tests/SoftwareEngineeringProblems.Tests/Problems/ArraysAndStrings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using SoftwareEngineeringProblems;
+
+namespace SoftwareEngineeringProblems.Tests
+{
+    public class ArrayAndStrings
+    {
+        [Fact]
+        public static void TestAreCharacterSetsEqual()
+        {
+            Assert.False(
+                ArraysAndStrings.AreCharacterSetsEqual("abcdef", "cabhsdfdhgf"));
+            Assert.True(
+                ArraysAndStrings.AreCharacterSetsEqual("abcffffffffffed", "fbcade"));
+            Assert.True(
+                ArraysAndStrings.AreCharacterSetsEqual("abbbcf     ed", "fbfffcade "));
+            Assert.True(
+                ArraysAndStrings.AreCharacterSetsEqual("abcf   ed", "fbcade "));
+        }
+
+        [Fact]
+        public static void TestReplaceSpaces0()
+        {
+            var originalStr = "abc d e f";
+            var strExpected = "abc%20d%20e%20f";
+
+            var newStrArray = new char[strExpected.Length];
+            originalStr.ToCharArray().CopyTo(newStrArray, 0);
+
+            ArraysAndStrings.ReplaceSpaces0(newStrArray, originalStr.Length);
+            Assert.Equal(strExpected, new string(newStrArray));
+
+
+            originalStr = "abcdef";
+            strExpected = "abcdef";
+
+            newStrArray = new char[strExpected.Length];
+            originalStr.ToCharArray().CopyTo(newStrArray, 0);
+
+            ArraysAndStrings.ReplaceSpaces0(newStrArray, originalStr.Length);
+            Assert.Equal(strExpected, new string(newStrArray));
+        }
+    }
+}
