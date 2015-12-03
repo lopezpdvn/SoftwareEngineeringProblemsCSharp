623bab1177bda3ac3876aad2163b3cdd92d7aa44
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-12-02 20:23:27 -0600

    Tests for inherited methods accesibility and added tests shared context

--- src/SoftwareEngineeringProblems/Problems/ObjectOrientedProgramming.cs
+++ src/SoftwareEngineeringProblems/Problems/ObjectOrientedProgramming.cs
@@ -9,12 +9,27 @@ namespace SoftwareEngineeringProblems.ObjectOrientedProgramming
     {
         public char SomeNonVirtualProperty => 'A';
         public virtual char SomeVirtualProperty => 'A';
+
+        private char APrivateMethod()
+        {
+            return 'A';
+        }
+
+        protected char AAnInternalMethod()
+        {
+            return 'A';
+        }
     }
 
     public class B : A
     {
         public new char SomeNonVirtualProperty => 'B';
         public override char SomeVirtualProperty => 'B';
+
+        public char APublicMethod()
+        {
+            return AAnInternalMethod();
+        }
     }
 
     public class C : B
--- tests/SoftwareEngineeringProblems.Tests/Problems/ObjectOrientedProgramming.cs
+++ tests/SoftwareEngineeringProblems.Tests/Problems/ObjectOrientedProgramming.cs
@@ -7,16 +7,32 @@ using SoftwareEngineeringProblems.ObjectOrientedProgramming;
 
 namespace SoftwareEngineeringProblems.Tests.ObjectOrientedProgramming
 {
-    public class ObjectOrientedProgrammingTests
+    public class ObjectsFixture
     {
+        // C is-a B is-a A
+        public A a = new A();
+        public B b = new B();
+        public C c = new C();
+    }
+
+    public class ObjectOrientedProgrammingTests : IClassFixture<ObjectsFixture>
+    {
+        ObjectsFixture fixture;
+        private A a;
+        private B b;
+        private C c;
+
+        public ObjectOrientedProgrammingTests(ObjectsFixture fixture)
+        {
+            this.fixture = fixture;
+            a = fixture.a;
+            b = fixture.b;
+            c = fixture.c;
+        }
+
         [Fact]
         public void VirtualMethods()
         {
-            // C is-a B is-a A
-            A a = new A();
-            B b = new B();
-            C c = new C();
-
             Assert.True(a.SomeNonVirtualProperty == 'A');
             Assert.True(b.SomeNonVirtualProperty == 'B');
             Assert.True(c.SomeNonVirtualProperty == 'C');
@@ -31,6 +47,12 @@ namespace SoftwareEngineeringProblems.Tests.ObjectOrientedProgramming
             Assert.True(((B)c).SomeVirtualProperty == 'C');
             Assert.True(((A)c).SomeVirtualProperty == 'C');
             Assert.True(((A)b).SomeVirtualProperty == 'B');
+        }
+
+        [Fact]
+        public void InheritedMethodAccesibility()
+        {
+            Assert.True(b.APublicMethod() == 'A');
         }
     }
 }
