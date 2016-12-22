b9fd1d749c6d3a41095bf8ba0a3be477a0b27c93
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-12-22 05:21:49 -0600

    Added misc collection tests

--- tests/SoftwareEngineeringProblems.Tests/Problems/Misc.cs
+++ tests/SoftwareEngineeringProblems.Tests/Problems/Misc.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
-using SoftwareEngineeringProblems;
 
 namespace SoftwareEngineeringProblems.Tests
 {
@@ -25,6 +22,38 @@ namespace SoftwareEngineeringProblems.Tests
             }
 
             //Assert.True(false);
+        }
+    }
+
+    public class CollectionMisc
+    {
+        [Fact]
+        public void TestDynamicArrayList()
+        {
+            List<string> daStr = new List<string>();
+            daStr.Add("one");
+            daStr.Add("three");
+            daStr.Insert(0, "zero");
+            daStr.Insert(2, "two");
+            daStr.Add("four");
+            Assert.Equal(daStr.Count, 5);
+            Assert.True(daStr.Contains("one"));
+            daStr.RemoveAt(1);
+            Assert.False(daStr.Contains("one"));
+            Assert.Equal(daStr[1], "two");
+            Assert.Equal(daStr.IndexOf("three"), 2);
+            Assert.Equal(daStr.Count, 4);
+            string[] ans = new string[] {"zero", "two", "three", "four"};
+            var i = 0;
+            foreach(var str in daStr) {
+                Assert.Equal(ans[i++], str);
+            }
+            daStr[1] = "dos";
+            ans = new string[] {"zero", "dos", "three", "four"};
+            i = 0;
+            foreach(var str in daStr) {
+                Assert.Equal(ans[i++], str);
+            }
         }
     }
 }
